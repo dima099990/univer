b348c9b0ec325598734ffdc33a12e0c9fe138498
dima099990 <dima099990@mail.ru>
2023-03-23 11:05:02 +0300

    Completed 3.1 lab

---
+++ lab3/main.py
@@ -0,0 +1,14 @@
+x = int(input('Введите X: '))
+y = int(input('Введите Y: '))
+z = int(input('Введите Z: '))
+
+if x == y:
+    print("x=y")
+elif y == z:
+    print("y=z")
+elif z == x:
+    print("z=x")
+else:
+    print("Переменные:  X,Y,Z попарно не равны!")
+
+##test kak eto rabotaet)))
