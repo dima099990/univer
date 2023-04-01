68fcede42e8a914698bfdedddcf28e3074d54bce
dima099990 <dima099990@mail.ru>
2023-04-01 10:19:13 +0300

    lab 6 completed

--- lab6/main.py
+++ lab6/main.py
@@ -5,6 +5,7 @@ d = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 b = int(input("Введите число b= : "))
 r = []
 x = float(0)
+sum=float(0)
 
 # for i in range(10):
 #     d.append(int(input(""'Введите число D['+str(i+1)+']')))
@@ -13,7 +14,23 @@ x = float(0)
 for i in range(len(d)):
     x = (((math.tan(d[i]) ** 2)) - math.tan(b * d[i])) / 2 * b + 1
     r.append(round(x, 3))
-    # print(r[i])
+    if i % 2 == 0:
+        sum=sum+r[i]
+# print(r[i])
+maximum = max(r)
+minimum = min(r)
 
-print(d)
 print(r)
+for i in range(len(r)):
+  if r[i] == maximum:
+    r[i] = minimum
+  elif r[i] == minimum:
+    r[i] = maximum
+
+
+
+# print(d)
+print(r)
+print('Сумма четных элементов',sum)
+
+
